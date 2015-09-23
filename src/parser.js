6f35fe798ad9e541249f82c2c83ac2ea224f62b3
Yongquan Lu <yqlu@mit.edu>
2015-09-23 18:59:21 -0400

    added code mirror with correct autocomplete styling

--- src/parser.js
+++ src/parser.js
@@ -1,57 +1,88 @@
 $(document).ready(function() {
-	var replacements = [
-		[/\\alpha/g, "α"],
-		[/\\beta/g, "β"],
-		[/\\gamma/g, "γ"],
-		[/\\delta/g, "δ"],
-		[/\\epsilon/g, "ε"],
-		[/\\zeta/g, "ζ"],
-		[/\\eta/g, "η"],
-		[/\\theta/g, "θ"],
-		[/\\iota/g, "ι"],
-		[/\\kappa/g, "κ"],
-		[/\\lambda/g, "λ"],
-		[/\\mu/g, "μ"],
-		[/\\nu/g, "ν"],
-		[/\\xi/g, "ξ"],
-		[/\\omicron/g, "ο"],
-		[/\\pi/g, "π"],
-		[/\\rho/g, "ρ"],
-		[/\\sigma/g, "σ"],
-		[/\\tau/g, "τ"],
-		[/\\upsilon/g, "υ"],
-		[/\\phi/g, "φ"],
-		[/\\chi/g, "χ"],
-		[/\\psi/g, "ψ"],
-		[/\\omega/g, "ω"],
-		[/\|->/g,"↦"]];
 
-	var parseAndDisplay = function() {
-		var p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
-		try {
-			var parsed = p.feed($("#input").val());
-			console.assert(parsed.results.length === 1, parsed.results);
-			$("#results").text(JSON.stringify(parsed.results[0], null, 0));
-			$("#resultstabbed").text(JSON.stringify(parsed.results[0], null, 4));
-		} catch (err) {
-			$("#results").text(err);
-		}
-	};
+    var parseAndDisplay = function() {
+        var p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
+        try {
+            var parsed = p.feed(cm.getValue());
+            console.assert(parsed.results.length === 1, parsed.results);
+            $("#results").text(JSON.stringify(parsed.results[0], null, 0));
+            $("#resultstabbed").text(JSON.stringify(parsed.results[0], null, 4));
+        } catch (err) {
+            $("#results").text(err);
+        }
+    };
 
-	$("#input").keyup(function(e) {
+    $("#submit").click(parseAndDisplay);
 
-		var text = $("#input").val();
-		for (var i = 0; i < replacements.length; i++) {
-			text = text.replace(replacements[i][0], replacements[i][1]);
-		}
-		$("#input").val(text);
+    var words = [
+        {text: "α", displayText: "\\alpha"},
+        {text: "β", displayText: "\\beta"},
+        {text: "γ", displayText: "\\gamma"},
+        {text: "δ", displayText: "\\delta"},
+        {text: "ε", displayText: "\\epsilon"},
+        {text: "ζ", displayText: "\\zeta"},
+        {text: "η", displayText: "\\eta"},
+        {text: "θ", displayText: "\\theta"},
+        {text: "ι", displayText: "\\iota"},
+        {text: "κ", displayText: "\\kappa"},
+        {text: "λ", displayText: "\\lambda"},
+        {text: "μ", displayText: "\\mu"},
+        {text: "ν", displayText: "\\nu"},
+        {text: "ξ", displayText: "\\xi"},
+        {text: "ο", displayText: "\\omicron"},
+        {text: "π", displayText: "\\pi"},
+        {text: "ρ", displayText: "\\rho"},
+        {text: "σ", displayText: "\\sigma"},
+        {text: "τ", displayText: "\\tau"},
+        {text: "υ", displayText: "\\upsilon"},
+        {text: "φ", displayText: "\\phi"},
+        {text: "χ", displayText: "\\chi"},
+        {text: "ψ", displayText: "\\psi"},
+        {text: "ω", displayText: "\\omega"},
+        {text: "↦", displayText: "|->"},
+        {text: "×", displayText: "\\times"},
+        {text: "∩", displayText: "\\cap"}];
 
-		// if pressed enter
-		if (13 === e.keyCode) {
-			parseAndDisplay();
-		}
-	});
+    CodeMirror.registerHelper("hint", "anyword", function(editor, options) {
+        var delimiters = /[\\|]/;
+        var whitespace = /\s/;
+        var cur = editor.getCursor(), curLine = editor.getLine(cur.line);
+        var start = cur.ch, end = start;
+        while (end < curLine.length && !whitespace.test(curLine.charAt(end))) {
+            end ++;
+        }
+        while (start >= 1 && !delimiters.test(curLine.charAt(start)) && !whitespace.test(curLine.charAt(start-1))) {
+            start --;
+        }
+        var curWord = start != end ? curLine.slice(start, end) : "";
 
-	$("#submit").click(parseAndDisplay);
+        var filteredWords = words.filter(function(w) {return curWord.length > 0 && w.displayText.indexOf(curWord) === 0;});
 
+        return {list: filteredWords, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
+    });
+
+    CodeMirror.commands.autocomplete = function(cm) {
+        cm.showHint({hint: CodeMirror.hint.anyword, completeSingle: false});
+    };
+
+    var cm = CodeMirror.fromTextArea($("#code")[0], {
+      mode:  "scheme",
+      theme: "material"
+    });
+
+    cm.on('keyup', function(cm, e){
+        var keycode = e.keyCode;
+
+        var valid =
+            (keycode > 47 && keycode < 58)   || // number keys
+            keycode == 32 || keycode == 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+            (keycode > 64 && keycode < 91)   || // letter keys
+            (keycode > 95 && keycode < 112)  || // numpad keys
+            (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+            (keycode > 218 && keycode < 223);   // [\]' (in order)
+
+        if (valid) {
+            CodeMirror.commands.autocomplete(cm);
+        }
+    });
 });
