1f0d13698295e630792ba2f464cc1a7e88ed63f5
Rohit Singh <theone.rohit@gmail.com>
2015-10-27 09:08:17 -0700

    code generated for prob c

---
+++ codegen_c/paren-c.cpp
@@ -0,0 +1,84 @@
+#include <cstdio>
+#include <cstdlib>
+#ifndef TYPE
+#define TYPE int
+#endif
+
+/* Min Max and Weight Function */
+#define min(a,b) (a<b?a:b)
+#define max(a,b) (a>b?a:b)
+#define w(i, j, k) (((i*j*k)&7)) //weight function
+
+#define N 100
+
+int B;
+TYPE [N][N] dist;
+
+#define FOR(i,n,m) for(int i=n;i<m;i++)
+#define BASE_CONSTRAINT(a,b,c,d,e,f) (b-a <= B && d-c<= B && f-e <= B)
+//DEFINITIONS and HEADERS
+void funcC_loop(int K0_start, int K0_end, int K1_start, int K1_end,
+		int K2_start, int K2_end) {
+
+	FOR(i, K0_start, K0_end)
+	{
+		FOR(j, K2_start, K2_end)
+		{
+
+			TYPE t14 = MAXVAL;
+			FOR(k, K1_start, K1_end)
+			{
+				t14 = min(t14, dist[ui][uk] + dist[uk][uj] + w(i, k, j));
+			}
+
+			dist[ui][uj] = min(t14, dist[ui][uj]);
+		}
+	}
+
+}
+
+void funcC_rec(int K0_start, int K0_end, int K1_start, int K1_end, int K2_start,
+		int K2_end) {
+	if (BASE_CONSTRAINT(K0_start, K0_end, K1_start, K1_end, K2_start, K2_end)) {
+		funcC_loop(K0_start, K0_end, K1_start, K1_end, K2_start, K2_end);
+		return;
+	}
+	int L0_start;
+	int L0_end;
+	int L1_start;
+	int L1_end;
+	int L2_start;
+	int L2_end;
+	int L3_start;
+	int L3_end;
+	int L4_start;
+	int L4_end;
+	int L5_start;
+	int L5_end;
+	L4_start = K2_start;
+	L4_end = (K2_end - _start) / 2;
+	L5_start = L4_end;
+	L5_end = K2_end;
+	L2_start = K1_start;
+	L2_end = (K1_end - _start) / 2;
+	L3_start = L2_end;
+	L3_end = K1_end;
+	L0_start = K0_start;
+	L0_end = (K0_end - _start) / 2;
+	L1_start = L0_end;
+	L1_end = K0_end;
+
+	funcC_rec(L0_start, L0_end, L2_start, L2_end, L4_start, L4_end);
+	funcC_rec(L0_start, L0_end, L3_start, L3_end, L4_start, L4_end);
+	funcC_rec(L0_start, L0_end, L2_start, L2_end, L5_start, L5_end);
+	funcC_rec(L0_start, L0_end, L3_start, L3_end, L5_start, L5_end);
+	funcC_rec(L1_start, L1_end, L2_start, L2_end, L4_start, L4_end);
+	funcC_rec(L1_start, L1_end, L3_start, L3_end, L4_start, L4_end);
+	funcC_rec(L1_start, L1_end, L2_start, L2_end, L5_start, L5_end);
+	funcC_rec(L1_start, L1_end, L3_start, L3_end, L5_start, L5_end);
+}
+
+int main(int argc, char *argv[]) {
+	B=0;
+}
+
