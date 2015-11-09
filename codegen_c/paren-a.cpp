4eb17464fa781ac25a62bb7c85a6471b4845be97
Rohit Singh <theone.rohit@gmail.com>
2015-11-09 08:42:38 -0800

    added code for experimenting parenthesis problem

--- codegen_c/paren-a.cpp
+++ codegen_c/paren-a.cpp
@@ -1,18 +1,31 @@
 /*
- * Run as: g++ -D N=100 -D B=5 -o parena paren-a.cpp
- * ./parena random
- * ./parena optimal
+ * g++ -O3 -D N=2000 -D B=16 -o parena paren-a.cpp  && ./parena standard A-rec-debug
+ *
  */
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <cassert>
 #include <iostream>
+#include "timerclass.h"
 using namespace std;
 #ifndef TYPE
 #define TYPE int
 #endif
 
+#ifdef INTINTERVAL
+#define DEFINTERVALFUNC(I) TYPE I##_begin, TYPE I##_end
+#define DEFINTERVALSTMT(I) TYPE I##_begin, I##_end
+#define DEFBEGIN(I) I##_begin
+#define DEFEND(I) I##_end
+#define PARAM(I) I##_begin, I##_end
+#else
+#define DEFINTERVALFUNC(I) interval I
+#define DEFINTERVALSTMT(I) interval I
+#define DEFBEGIN(I) I.begin
+#define DEFEND(I) I.end
+#define PARAM(I) I
+#endif
 #define MAXVAL 100000
 
 /* Min Max and Weight Function */
@@ -35,16 +48,16 @@ struct interval {
 #define FOR_VAR_FWD(i,n,m) for(TYPE i=n;i<m;i++)
 #define FOR_VAR_BWD(i,n,m) for(TYPE i=m-1;i>=n;i--)
 
-#define SIZE(I) (I.end-I.begin)
-#define FOR_FORWARD(i,K) for(TYPE i=K.begin;i<K.end;i++)
-#define FOR_BACKWARD(i,K) for(TYPE i=K.end-1;i>=K.begin;i--)
+#define SIZE(I) (DEFEND(I)-DEFBEGIN(I))
+#define FOR_FORWARD(i,K) for(TYPE i=DEFBEGIN(K);i<DEFEND(K);i++)
+#define FOR_BACKWARD(i,K) for(TYPE i=DEFEND(K)-1;i>=DEFBEGIN(K);i--)
 #define FOR_FWD_FWD(i,j,I,J,ZZ) FOR_FORWARD(i,I){FOR_FORWARD(j,J){ZZ}}
 #define FOR_FWD_BWD(i,j,I,J,ZZ) FOR_FORWARD(i,I){FOR_BACKWARD(j,J){ZZ}}
 #define FOR_BWD_FWD(i,j,I,J,ZZ) FOR_BACKWARD(i,I){FOR_FORWARD(j,J){ZZ}}
 #define FOR_BWD_BWD(i,j,I,J,ZZ) FOR_BACKWARD(i,I){FOR_BACKWARD(j,J){ZZ}}
 
 //SIZE(I) == SIZE(J)
-#define FOR_DIAG_I_LT_J_FWD_FWD(i,j,I,J,ZZ) FOR_VAR_FWD(of,0,SIZE(I)){FOR_VAR_FWD(ci,0,SIZE(J)-of){TYPE i = ci+I.begin; TYPE j = J.begin+ci+of; ZZ}}
+#define FOR_DIAG_I_LT_J_FWD_FWD(i,j,I,J,ZZ) FOR_VAR_FWD(of,0,SIZE(I)){FOR_VAR_FWD(ci,0,SIZE(J)-of){TYPE i = ci+DEFBEGIN(I); TYPE j = DEFBEGIN(J)+ci+of; ZZ}}
 
 #define FOR_A_loop_1(i,K) FOR_FORWARD(i,K)
 #define FOR_A_loop_2(i,j,I,J,ZZ) FOR_DIAG_I_LT_J_FWD_FWD(i,j,I,J,ZZ)
@@ -55,104 +68,118 @@ struct interval {
 #define FOR_C_loop_2(i,j,I,J,ZZ) FOR_FWD_FWD(i,j,I,J,ZZ)
 
 #define FORUNION(i,K,L,ZZ) FOR_FORWARD(i,K){ZZ};FOR_FORWARD(i,L){ZZ}
-#define BASE_CONSTRAINT_A(a) ((a.end-a.begin) <= B)
+#define BASE_CONSTRAINT_A(a) ((DEFEND(a)-DEFBEGIN(a)) <= B)
 #define BASE_CONSTRAINT_B(a,b) (BASE_CONSTRAINT_A(a) || BASE_CONSTRAINT_A(b))
 #define BASE_CONSTRAINT_C(a,b,c) (BASE_CONSTRAINT_B(a,b) || BASE_CONSTRAINT_A(c))
-string distType = "random";
-
+string distType = "";
+string runType = "";
 /*
  * Auto-generated Code
  */
-
-void funcC_loop(interval K0, interval K1, interval K2) {
+void funcC_loop(DEFINTERVALFUNC(K0), DEFINTERVALFUNC(K1), DEFINTERVALFUNC(K2)) {
 
 	FOR_C_loop_2(i, j, K0, K2,
 
-			TYPE t14= MAXVAL; FOR_C_loop_1(k,K1){ if (i<k && k<j){ t14 = min(t14,dist[i][k]+dist[k][j]+w(i,k,j)); } }
+			TYPE t310= MAXVAL; FOR_C_loop_1(k,K1){ if (i<k && k<j){ t310 = min(t310,dist[i][k]+dist[k][j]+w(i,k,j)); } }
 
-			dist[i][j] = min(t14,dist[i][j]);
-
-			)
+			dist[i][j] = min(t310,dist[i][j]);)
 
 }
-void funcC_rec(interval K0, interval K1, interval K2) {
+void funcC_rec(DEFINTERVALFUNC(K0), DEFINTERVALFUNC(K1), DEFINTERVALFUNC(K2)) {
 	if (BASE_CONSTRAINT_C(K0, K1, K2)) {
-		funcC_loop(K0, K1, K2);
+		funcC_loop(PARAM(K0), PARAM(K1), PARAM(K2));
 		return;
 	}
-	interval L4 = { K2.begin, (K2.end + K2.begin) / 2 };
-	interval L5 = { L4.end, K2.end };
-	interval L2 = { K1.begin, (K1.end + K1.begin) / 2 };
-	interval L3 = { L2.end, K1.end };
-	interval L0 = { K0.begin, (K0.end + K0.begin) / 2 };
-	interval L1 = { L0.end, K0.end };
+	DEFINTERVALSTMT(L4);
+	DEFBEGIN(L4) = DEFBEGIN(K2);
+	DEFEND(L4) = (DEFEND(K2) + DEFBEGIN(K2)) / 2;
+	DEFINTERVALSTMT(L5);
+	DEFBEGIN(L5) = DEFEND(L4);
+	DEFEND(L5) = DEFEND(K2);
+	DEFINTERVALSTMT(L2);
+	DEFBEGIN(L2) = DEFBEGIN(K1);
+	DEFEND(L2) = (DEFEND(K1) + DEFBEGIN(K1)) / 2;
+	DEFINTERVALSTMT(L3);
+	DEFBEGIN(L3) = DEFEND(L2);
+	DEFEND(L3) = DEFEND(K1);
+	DEFINTERVALSTMT(L0);
+	DEFBEGIN(L0) = DEFBEGIN(K0);
+	DEFEND(L0) = (DEFEND(K0) + DEFBEGIN(K0)) / 2;
+	DEFINTERVALSTMT(L1);
+	DEFBEGIN(L1) = DEFEND(L0);
+	DEFEND(L1) = DEFEND(K0);
 
-	funcC_rec(L0, L2, L4);
-	funcC_rec(L0, L3, L4);
-	funcC_rec(L0, L2, L5);
-	funcC_rec(L0, L3, L5);
-	funcC_rec(L1, L2, L4);
-	funcC_rec(L1, L3, L4);
-	funcC_rec(L1, L2, L5);
-	funcC_rec(L1, L3, L5);
+	funcC_rec(PARAM(L0), PARAM(L2), PARAM(L4));
+	funcC_rec(PARAM(L0), PARAM(L3), PARAM(L4));
+	funcC_rec(PARAM(L0), PARAM(L2), PARAM(L5));
+	funcC_rec(PARAM(L0), PARAM(L3), PARAM(L5));
+	funcC_rec(PARAM(L1), PARAM(L2), PARAM(L4));
+	funcC_rec(PARAM(L1), PARAM(L3), PARAM(L4));
+	funcC_rec(PARAM(L1), PARAM(L2), PARAM(L5));
+	funcC_rec(PARAM(L1), PARAM(L3), PARAM(L5));
 }
 
-void funcB_loop(interval J0, interval J1) {
+void funcB_loop(DEFINTERVALFUNC(J0), DEFINTERVALFUNC(J1)) {
 
 	FOR_B_loop_1(i, j, J0, J1,
 
 			TYPE t21= MAXVAL; FORUNION(k,J0,J1, if (i<k && k<j){ t21 = min(t21,dist[i][k]+dist[k][j]+w(i,k,j)); } )
 
-			dist[i][j] = min(t21,dist[i][j]);
-
-			)
+			dist[i][j] = min(t21,dist[i][j]);)
 
 }
-void funcB_rec(interval J0, interval J1) {
+void funcB_rec(DEFINTERVALFUNC(J0), DEFINTERVALFUNC(J1)) {
 	if (BASE_CONSTRAINT_B(J0, J1)) {
-		funcB_loop(J0, J1);
+		funcB_loop(PARAM(J0), PARAM(J1));
 		return;
 	}
-	interval K0 = { J0.begin, (J0.end + J0.begin) / 2 };
-	interval K1 = { K0.end, J0.end };
-	interval K2 = { J1.begin, (J1.end + J1.begin) / 2 };
-	interval K3 = { K2.end, J1.end };
+	DEFINTERVALSTMT(K0);
+	DEFBEGIN(K0) = DEFBEGIN(J0);
+	DEFEND(K0) = (DEFEND(J0) + DEFBEGIN(J0)) / 2;
+	DEFINTERVALSTMT(K1);
+	DEFBEGIN(K1) = DEFEND(K0);
+	DEFEND(K1) = DEFEND(J0);
+	DEFINTERVALSTMT(K2);
+	DEFBEGIN(K2) = DEFBEGIN(J1);
+	DEFEND(K2) = (DEFEND(J1) + DEFBEGIN(J1)) / 2;
+	DEFINTERVALSTMT(K3);
+	DEFBEGIN(K3) = DEFEND(K2);
+	DEFEND(K3) = DEFEND(J1);
 
-	funcB_rec(K1, K2);
-	funcC_rec(K0, K1, K2);
-	funcB_rec(K0, K2);
-	funcC_rec(K1, K2, K3);
-	funcB_rec(K1, K3);
-	funcC_rec(K0, K1, K3);
-	funcC_rec(K0, K2, K3);
-	funcB_rec(K0, K3);
+	funcB_rec(PARAM(K1), PARAM(K2));
+	funcC_rec(PARAM(K0), PARAM(K1), PARAM(K2));
+	funcB_rec(PARAM(K0), PARAM(K2));
+	funcC_rec(PARAM(K1), PARAM(K2), PARAM(K3));
+	funcB_rec(PARAM(K1), PARAM(K3));
+	funcC_rec(PARAM(K0), PARAM(K1), PARAM(K3));
+	funcC_rec(PARAM(K0), PARAM(K2), PARAM(K3));
+	funcB_rec(PARAM(K0), PARAM(K3));
 }
 
-void funcA_loop(interval J) {
+void funcA_loop(DEFINTERVALFUNC(J)) {
 
 	FOR_A_loop_2(i, j, J, J,
 
-			TYPE t16= MAXVAL;
+			TYPE t504= MAXVAL; FOR_A_loop_1(k,J){ if (i<k && k<j){ t504 = min(t504,dist[i][k]+dist[k][j]+w(i,k,j)); } }
 
-			FOR_A_loop_1(k,J){ if (i<k && k<j){ t16 = min(t16,dist[i][k]+dist[k][j]+w(i,k,j)); } }
-
-			dist[i][j] = min(t16,dist[i][j]);)
+			dist[i][j] = min(t504,dist[i][j]);)
 
 }
-void funcA_rec(interval J) {
-	//cout<<"recA:\t["<<J.begin<<"\t"<<J.end<<")"<<endl;
+void funcA_rec(DEFINTERVALFUNC(J)) {
 	if (BASE_CONSTRAINT_A(J)) {
-		//cout<<"recA calling loopA:\t["<<J.begin<<"\t"<<J.end<<")"<<endl;
-
-		funcA_loop(J);
+		funcA_loop(PARAM(J));
 		return;
 	}
-	interval J0 = { J.begin, (J.end + J.begin) / 2 };
-	interval J1 = { J0.end, J.end };
+	DEFINTERVALSTMT(J0);
+	DEFBEGIN(J0) = DEFBEGIN(J);
+	DEFEND(J0) = (DEFEND(J) + DEFBEGIN(J)) / 2;
+	DEFINTERVALSTMT(J1);
+	DEFBEGIN(J1) = DEFEND(J0);
+	DEFEND(J1) = DEFEND(J);
 
-	funcA_rec(J0);
-	funcA_rec(J1);
-	funcB_rec(J0, J1);
+	funcA_rec(PARAM(J0));
+	funcA_rec(PARAM(J1));
+	funcB_rec(PARAM(J0), PARAM(J1));
 }
 
 void parenthesis() {
@@ -222,24 +249,29 @@ void checkLoopRec(string func) {
 				ctr++;
 		}
 	}
-	cout << ctr << " values updated by both " << func << " functions correctly."
-			<< endl;
+	cout << func << " " << ctr << " values updated correctly." << endl;
 	//cout << "DONE" << endl;
 }
 
 void testC() {
 
-	interval K0 = { 0, N / 3 };
-	interval K1 = { K0.end, (2 * N) / 3 };
-	interval K2 = { K1.end, N };
+	DEFINTERVALSTMT(K0);
+	DEFBEGIN(K0) = 0;
+	DEFEND(K0) = N / 3;
+	DEFINTERVALSTMT(K1);
+	DEFBEGIN(K1) = DEFEND(K0);
+	DEFEND(K1) = (2 * N) / 3;
+	DEFINTERVALSTMT(K2);
+	DEFBEGIN(K2) = DEFEND(K1);
+	DEFEND(K2) = N;
 	dcopy(dorig, dist);
-	funcC_loop(K0, K1, K2);
+	funcC_loop(PARAM(K0), PARAM(K1), PARAM(K2));
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, dloop);
 
 	dcopy(dorig, dist);
-	funcC_rec(K0, K1, K2);
+	funcC_rec(PARAM(K0), PARAM(K1), PARAM(K2));
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, drec);
@@ -248,16 +280,20 @@ void testC() {
 
 void testB() {
 
-	interval K0 = { 0, N / 2 };
-	interval K1 = { K0.end, N };
+	DEFINTERVALSTMT(K0);
+	DEFBEGIN(K0) = 0;
+	DEFEND(K0) = N / 2;
+	DEFINTERVALSTMT(K1);
+	DEFBEGIN(K1) = DEFEND(K0);
+	DEFEND(K1) = N;
 	dcopy(dorig, dist);
-	funcB_loop(K0, K1);
+	funcB_loop(PARAM(K0), PARAM(K1));
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, dloop);
 
 	dcopy(dorig, dist);
-	funcB_rec(K0, K1);
+	funcB_rec(PARAM(K0), PARAM(K1));
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, drec);
@@ -266,18 +302,27 @@ void testB() {
 
 void testA() {
 
-	interval K0 = { 0, N };
+	DEFINTERVALSTMT(K0);
+	DEFBEGIN(K0) = 0;
+	DEFEND(K0) = N;
 	if (N < 20)
 		cout << "loop run:" << endl;
 	dcopy(dorig, dist);
-	funcA_loop(K0);
+	timerclass tc;
+	tc.start();
+	funcA_loop(PARAM(K0));
+	tc.stop();
+	tc.print("A-loop");
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, dloop);
 	if (N < 20)
 		cout << "rec run:" << endl;
 	dcopy(dorig, dist);
-	funcA_rec(K0);
+	tc.restart();
+	funcA_rec(PARAM(K0));
+	tc.stop();
+	tc.print("A-rec");
 	if (N < 20)
 		print_dist(dorig);
 	dcopy(dist, drec);
@@ -292,15 +337,38 @@ void testA() {
 	checkLoopRec("funcA");
 }
 
-int main(int argc, char *argv[]) {
-	srand (time(NULL));
+inline void testArec(bool debug) {
+	DEFINTERVALSTMT(K0);
+	DEFBEGIN(K0) = 0;
+	DEFEND(K0) = N;
+	dcopy(dorig, dist);
+	timerclass tc;
+	tc.start();
+	funcA_rec(PARAM(K0));
+	tc.stop();
+	tc.print("A-rec");
+	if (debug) {
+		dcopy(dist, drec);
+		dcopy(dorig, dist);
+		timerclass tc;
+		tc.start();
+		parenthesis();
+		tc.stop();
+		tc.print("paren");
+		dcopy(dist, dparen);
 
-if	(argc > 1) {
-		distType = argv[1];
+		dcopy(drec, dloop);
+		checkLoopRec("DEBUG A-rec vs paren");
 	}
+}
+int main(int argc, char *argv[]) {
+	srand (time(NULL));if (argc != 3) {
+		cout<<"Need two arguments: distType{random,standard} runType{A,B,C,A-rec}"<<endl;
+	}
+	distType = argv[1];
+	runType = argv[2];
 	if (distType == "random") {
-		cout<<"Random dist: "<<endl;
-
+		cout<<"Random dist "<<endl;
 		for (int i=0;i<N;i++) {
 			for (int j=0;j<N;j++) {
 				if (i<j) {
@@ -313,8 +381,8 @@ if	(argc > 1) {
 			}
 		}
 	}
-	else if (distType == "optimal") {
-		cout<<"Optimal dist: "<<endl;
+	else if (distType == "standard") {
+		cout<<"Standard dist "<<endl;
 
 		for (int i=0;i<N;i++) {
 			for (int j=0;j<N;j++) {
@@ -328,44 +396,31 @@ if	(argc > 1) {
 			}
 		}
 	}
-	else if (distType == "fixed5") {
-		cout<<"Fixed dist: "<<endl;
-		if (N!=5) {
-			cout<<"Error: N must be 5!"<<endl;
-			exit(1);
-		}
-		for (int i=0;i<N;i++) {
-			for (int j=0;j<N;j++) {
-				dorig[i][j] = MAXVAL;
-			}
-		}
-		dorig[0][1]=19;
-		dorig[0][2]=0;
-		dorig[0][3]=14;
-		dorig[0][4]=-14;
-		dorig[1][2]=-10;
-		dorig[1][3]=5;
-		dorig[1][4]=-12;
-		dorig[2][3]=-7;
-		dorig[2][4]=14;
-		dorig[3][4]=-12;
-		for (int i=0;i<N;i++) {
-			for (int j=0;j<N;j++) {
-				if (i<j) cout<<i<<'\t'<<j<<'\t'<<dorig[i][j]<<endl;
-			}
-		}
-	}
 	else {
-		cout<<"Error: Invalid distType"<<endl;
+		cout<<"Error: Invalid distType: "<<distType<<endl;
 		exit(1);
 	}
 
-	if (distType != "fixed5") {
+	if (runType == "C") {
 		cout<<"funcC test: "<<endl;
 		testC();
+	}
+	else if (runType == "B") {
 		cout<<"funcB test: "<<endl;
 		testB();
 	}
-	cout<<"funcA test: "<<endl;
-	testA();
+	else if (runType == "A") {
+		cout<<"funcA test: "<<endl;
+		testA();
+	}
+	else if (runType == "A-rec") {
+		testArec(false);
+	}
+	else if (runType == "A-rec-debug") {
+		testArec(true);
+	}
+	else {
+		cout<<"Invalid testType: "<<runType<<endl;
+		exit(1);
+	}
 }
