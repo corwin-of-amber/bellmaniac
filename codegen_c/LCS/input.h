1d3fe0b8104c5ca948c3d5595eb306dfdbdd1964
corwin <corwin.amber@gmail.com>
2016-06-16 13:42:18 +0300

    QA: LCS json.

--- codegen_c/LCS/input.h
+++ codegen_c/LCS/input.h
@@ -3,3 +3,10 @@ extern int *X, *Y;
 inline bool delta(int i, int j) { return X[i] == Y[j]; }
 
 inline int oplus(int i, int j) { return i + j; }
+
+
+#define J___J1_J1minus1_begin J1_begin
+#define J___J1_J1minus1_end J1_end
+#define I___I1_I1minus1_begin I1_begin
+#define I___I1_I1minus1_end I1_end
+
